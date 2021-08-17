6f3d239425db74eee4c1b3580864236f05da43a7
Jose Miranda <josemiranda8620@hotmail.com>
2021-08-17 12:29:06 +0900

    Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Offer.create(name: "Break up relationships", details: "I can help you avoid that difficult situation with that person you don't want to see anymore", price: 10000)
+Offer.create(name: "Fire an employee", details: "Quick and painless firing service on demand", price: 15000)
+Offer.create(name: "Deliver a citation", details: "If you don't want to deal with possible violent situations", price: 15000)
+Offer.create(name: "Deal with your noisy neighboor", details: "A confrontation that your neighboor will never forget", price: 20000)
+Offer.create(name: "Announce a recent death", details: "To ease the pain of the current lost I can help you do communicate it to others", price: 10000)
+Offer.create(name: "Present a complaint to a store", details: "If you don't want to go through the problems of getting your money back I can offer an easy solution", price: 10000)
+Offer.create(name: "OFfer Name", details: "Description of the offer", price: 10000)
+Offer.create(name: "OFfer Name", details: "Description of the offer", price: 10000)
+Offer.create(name: "OFfer Name", details: "Description of the offer", price: 10000)
+Offer.create(name: "OFfer Name", details: "Description of the offer", price: 10000)
+
