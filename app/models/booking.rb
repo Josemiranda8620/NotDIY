968415d3e0c4a950ab80a47a5420e7895d4567fc
Daniel Rabottini <da_nkr@live.com>
2021-08-19 15:41:56 +0900

    working on params still

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,9 @@ class Booking < ApplicationRecord
   belongs_to :offer
   enum status: [:pending, :confirmed, :rejected]
 
+  # validates :date, presence: true
+  # validates :reason, presence: true
+
   def pending?
     status == 'pending'
   end
