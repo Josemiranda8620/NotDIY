a693697ee982dadcaf4973b66c9b785e595d9883
Daniel Rabottini <da_nkr@live.com>
2021-08-20 12:49:22 +0900

    front end and seed completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :offers
-  has_many :bookings
+  has_many :offers, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_many :bookings_as_owner, through: :offers, source: :bookings
   has_one_attached :photo
 end
